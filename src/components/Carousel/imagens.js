e7d396a3e9b0d307d12953a67fa9a47c7c3ad7fc
Kelvia Santos <kelviaks@gmail.com>
2023-09-25 21:26:46 -0300

    feat: creates array of images to use in the carousel using map

---
+++ src/components/Carousel/imagens.js
@@ -0,0 +1,28 @@
+import recife from "../../Assets/Principal/recife.png";
+import bezerros from "../../Assets/Principal/bezerros.png";
+import nazareDaMata from "../../Assets/Principal/NazareDaMata.png";
+import caruaru from "../../Assets/Principal/Caruaru.png";
+import gravata from "../../Assets/Principal/Gravata.png";
+
+export const imagens = [
+  {
+    name: "Imagens de Recife",
+    image: recife,
+  },
+  {
+    name: "Papangus de Bezerros",
+    image: bezerros,
+  },
+  {
+    name: "Caboclo de Lança, Nazaré da Mata",
+    image: nazareDaMata,
+  },
+  {
+    name: "Festividade de São joão em Caruaru",
+    image: caruaru,
+  },
+  {
+    name: "Imagem da praça do relógio em Gravatá",
+    image: gravata,
+  },
+];
--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -1,56 +1,22 @@
 import Carousel from "react-bootstrap/Carousel";
 import "bootstrap/dist/css/bootstrap.css";
-import recife from "../../Assets/Principal/recife.png";
-import bezerros from "../../Assets/Principal/bezerros.png";
-import nazareDaMata from "../../Assets/Principal/NazareDaMata.png";
-import caruaru from "../../Assets/Principal/Caruaru.png";
-import gravata from "../../Assets/Principal/Gravata.png";
-import { Container } from "./styled"; 
+import { Container } from "./styled";
+import { imagens } from "./imagens";
 
 function CarouselHome({ description, title, detail }) {
   return (
     <Container>
       <Carousel fade>
-        <Carousel.Item>
-          <img src={recife} alt="Imagens de Recife" />
-          <Carousel.Caption>
-            <h4>{title}</h4>
-            <span>{detail}</span>
-            <p>{description}</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img src={bezerros} alt="Papangus de Bezerros" />
-          <Carousel.Caption>
-          <h4>{title}</h4>
-            <span>{detail}</span>
-            <p>{description}</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img src={nazareDaMata} alt="Caboclo de Lança, Nazaré da Mata" />
-          <Carousel.Caption>
-          <h4>{title}</h4>
-            <span>{detail}</span>
-            <p>{description}</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img src={caruaru} alt="Festividade de São joão em Caruaru" />
-          <Carousel.Caption>
-          <h4>{title}</h4>
-            <span>{detail}</span>
-            <p>{description}</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img src={gravata} alt="Imagem da praça do relógio em Gravatá" />
-          <Carousel.Caption>
-          <h4>{title}</h4>
-            <span>{detail}</span>
-            <p>{description}</p>
-          </Carousel.Caption>
-        </Carousel.Item>
+        {imagens.map(({ name, image }) => (
+          <Carousel.Item key={name}>
+            <img src={image} alt={name} />
+            <Carousel.Caption>
+              <h4>{title}</h4>
+              <span>{detail}</span>
+              <p>{description}</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        ))}
       </Carousel>
     </Container>
   );
