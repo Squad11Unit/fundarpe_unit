e7a9779736db26cf1e4ff695f6591a4aaac2fbdd
Beatriz Rodriegues <bealimaa.rodrigues@gmail.com>
2023-09-29 16:21:05 -0300

    feat: criar componente de card

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,7 @@
+import CardHome from "../../components/Card";
 import CarouselHome from "../../components/Carousel";
 import Header from "../../components/Header";
+import {H1} from "./styled";
 
 const Home = () => {
   return (
@@ -10,6 +12,8 @@ const Home = () => {
         detail="voltada para o apoio e promoção da cultura no estado"
         description="A Funcultura atua através de editais lançadas anualmente, essas oportunidades disponibilizam recursos do governo do Estado para artistas e produtores culturais para a realização de projetos em diversas áreas, visando fortalecer a cultura de Pernambuco."
       />
+      <H1>Novos Editais</H1>
+      <CardHome/>
     </div>
   );
 };
