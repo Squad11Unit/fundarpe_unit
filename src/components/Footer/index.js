11e55a2495670f43c1e43ebddf6df66534133061
Rennan <rennanreis2012@gmail.com>
2023-09-29 17:45:40 -0300

    adicionando a bandeira de pernambuco

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,10 +1,20 @@
 import { ContainerFooter } from "./styled";
-import rodape from "../../Assets/Footer/rodape.jpg"
+import rodape from "../../Assets/Footer/Barra colorida footer.png"
+import mapaPernambuco from "../../Assets/Footer/Mapa pernambuco footer.png"
 
 const Footer = () => (
     <ContainerFooter>
-        <img src={rodape} alt="barra colorida do rodapé"/>
+        
+            <div className="barra-colorida" >
+                <img src={rodape} alt="barra colorida do rodapé" className="colorido-rodape"/>
+            </div>
+            <div className="bandeira-pernambuco">
+                <img src={mapaPernambuco} alt="mapa de Pernambuco" className="mapa-pernambuco"/>
+            </div>
+        
+        
     </ContainerFooter>
+        
 );
 
 export default Footer;
--- src/components/Footer/styled.js
+++ src/components/Footer/styled.js
@@ -1,14 +1,32 @@
 import styled from "styled-components";
 
 export const ContainerFooter = styled.div`
+    *{
+        margin: 0;
+        padding: 0;
+    }
+
     width: 100%;
-    height: 485px;
+    height: 30.313rem;
     background-color: #2F3E52;
-   
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;   
+    
+    
 
-    img{
+    .barra-colorida > img{
+        display: flex;
+        justify-content: center;
         width: 100%;
-
+        height: 60.274px;
+    }
+    .bandeira-pernambuco > img{
+        display: flex;
+        justify-content: center;
+        width: 100%;
+        opacity: 0.25px;
+        
     }
 
 
