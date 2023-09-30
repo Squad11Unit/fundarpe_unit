7971ff6b3520a61431d65e5dce1139a1a3e215b2
Beatriz Silva <annbia24@gmail.com>
2023-09-30 15:32:09 -0300

    refactor: header component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,29 +1,32 @@
 import { useNavigate } from "react-router";
+import { HeaderContainer, InitialOptions } from "./styled";
+import logoheader from "../../Assets/logoheader.png";
+import { BsGear } from "react-icons/bs";
 
-const Header = () => {
-
+function Header() {
   const navigate = useNavigate();
-
-
   const ClearAll = () => {
-    window.localStorage.removeItem("User")
-    navigate("/")
-  }
-
-  return (
-    window.localStorage.getItem("User")
-      ?
-      <header style={{ display: "flex", justifyContent: "space-around" }}>
-        <p>editais</p>
-        <p>perfil</p>
-        <button onClick={() => ClearAll()}>Sair</button>
-      </header>
-      :
-      <header style={{ display: "flex", justifyContent: "space-around" }}>
-        <a href="/login">Login</a>
-        <a href="/cadastro">cadastro</a>
-
-      </header>
+    window.localStorage.removeItem("User");
+    navigate("/");
+  };
+  return window.localStorage.getItem("User") ? (
+    <HeaderContainer>
+      <img src={logoheader} alt="Logo do Funcultura" />
+      <InitialOptions>
+        <p>Instituições</p>
+        <p>Ana Silva</p>
+        <BsGear onClick={() => ClearAll()}/>
+      </InitialOptions>
+    </HeaderContainer>
+  ) : (
+    <HeaderContainer>
+      <img src={logoheader} alt="Logo do Funcultura" />
+      <InitialOptions>
+        <p>Instituições</p>
+        <a href="/login">Iniciar Sessão</a>
+        <button onClick={() => navigate("/cadastro")}>Registrar-se</button>
+      </InitialOptions>
+    </HeaderContainer>
   );
 }
 
---
+++ src/components/Header/styled.js
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+
+export const HeaderContainer = styled.div`
+  height: 90px;
+  width: 100%;
+  background-color: #011a3f;
+  color: #fdfeff;
+  display: flex;
+  justify-content: space-between;
+  padding: 3.5rem 115px;
+  align-items: center;
+  img {
+    height: 100px;
+  }
+`;
+export const InitialOptions = styled.div`
+  display: flex;
+  gap: 30px;
+  align-items: center;
+  p {
+    margin-top: 13px;
+  }
+  svg {
+    height: 25px;
+    width: 25px;
+  }
+  a {
+    text-decoration: none;
+    color: #fdfeff;
+  }
+  button {
+    border-radius: 50px;
+    background-color: #e31f28;
+    color: #fdfeff;
+    padding: 10px 20px;
+  }
+`;
