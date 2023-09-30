e207249aebce37c27b7ded6f05ffae0334815e77
Rennan <rennanreis2012@gmail.com>
2023-09-29 23:39:21 -0300

    adicionando conteudo do footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,18 +1,55 @@
 import { ContainerFooter } from "./styled";
-import rodape from "../../Assets/Footer/Barra colorida footer.png"
-import mapaPernambuco from "../../Assets/Footer/Mapa pernambuco footer.png"
+import background from "../../Assets/Footer/fundo.png"
+import rodape from "../../Assets/Footer/Barra colorida footer.png";
+import logosRodape from "../../Assets/Footer/logo-Fundarpe-pernambuco.png";
 
 const Footer = () => (
     <ContainerFooter>
+        <section className="fundo" style={{
+            width: '100%',
+            height: '630px',
+            backgroundImage: `url(${background})`
+        }}>
+
         
             <div className="barra-colorida" >
                 <img src={rodape} alt="barra colorida do rodapé" className="colorido-rodape"/>
             </div>
-            <div className="bandeira-pernambuco">
-                <img src={mapaPernambuco} alt="mapa de Pernambuco" className="mapa-pernambuco"/>
-            </div>
-        
-        
+            <section className="conteudo">
+                <div className="institucional">
+                    <h1>INSTITUCIONAL</h1>
+                    <a href="https://www.cultura.pe.gov.br/secultpe/"><li className="secretaria">Secretaria de cultura</li></a>
+                    <a href="https://www.cultura.pe.gov.br/fundarpe/"><li className="fundarpe">Fundarpe</li></a>
+                    <a href="https://www.cultura.pe.gov.br/funcultura/"><li className="funcultura">Funcultura</li></a>
+                </div>
+                <div className="linguagens">
+                    <h1>LINGUAGENS</h1>
+                    <a href="https://www.cultura.pe.gov.br/audiovisual/"><li className="audio-visual">Audio Visual</li></a>
+                    <a href="/"><li className="geral">Geral</li></a>
+                    <a href="/"><li className="microprojeto">Microprojeto</li></a>
+                    <a href="https://www.cultura.pe.gov.br/musica/"><li className="musica">Música</li></a>
+                </div>
+                <div className="suporte">
+                    <h1>SUPORTE</h1>
+                    <a href="https://www.cultura.pe.gov.br/pagina/expediente/"><li className="expediente">Expediente</li></a>
+                    <a href="https://www.cultura.pe.gov.br/pagina/ouvidoria/"><li className="ouvidoria">Ouvidoria</li></a>
+                    <a href="https://www.cultura.pe.gov.br/noticias/"><li className="noticias">Notícias</li></a>
+                    <a href="/"><li className="politica-privacidade">Política de privacidade</li></a>
+                </div>
+            </section>
+
+            {/* Na section abaixo está os icones e as logos da fundarpe e governo de pernambuco */}
+            <section className="ultimos-elementos">
+                <div className="logos-funpe">
+                    <img src={logosRodape} alt="logo de 50 anos da fundarpe e do governo de pernambuco" className="logos"/>
+                </div> 
+                <div>
+                
+                </div>
+            </section>
+            
+        </section> 
+
     </ContainerFooter>
         
 );
--- src/components/Footer/styled.js
+++ src/components/Footer/styled.js
@@ -1,34 +1,179 @@
 import styled from "styled-components";
 
 export const ContainerFooter = styled.div`
+    width: 100%;
+    justify-content: center;
+    
+
     *{
         margin: 0;
         padding: 0;
     }
-
-    width: 100%;
-    height: 30.313rem;
-    background-color: #2F3E52;
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: center;   
     
-    
-
+    .fundo{
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+    }
+       
     .barra-colorida > img{
         display: flex;
         justify-content: center;
         width: 100%;
         height: 60.274px;
     }
-    .bandeira-pernambuco > img{
-        display: flex;
-        justify-content: center;
-        width: 100%;
-        opacity: 0.25px;
+    .ultimos-elementos{
+        width: 1440px;
+        height: 133px;
+    }
+    .logos-funpe > img {
+        position: absolute;
+        bottom: 1px;
+        width: 42%;
+        height: 123px;
         
     }
 
+    .conteudo{
+        display: inline-flex;
+        
+        margin-between: 10px
+
+    }
+
+    .institucional{
+        margin-left: 2px
+        position: relative;
+        padding: 2rem;
+        align: right;
+        
+    }
+    .institucional > h1{
+        position: relative;
+        color: #FFF;
+        font-family: Open Sans;
+        font-size: 32px;
+        margin: 
+        
+
+    }
+    .secretaria {
+        color: #FFF;
+        font-family: Open Sans;
+        font-size: 22px;
+        font-style: normal;
+        font-weight: 300;
+        line-height: normal;
+    }
+    .fundarpe {
+        color: #FFF;
+        font-family: Open Sans;
+        font-size: 22px;
+        font-style: normal;
+        font-weight: 300;
+        line-height: normal;
+    }
+    .funcultura{
+        color: #FFF;
+        font-family: Open Sans;
+        font-size: 22px;
+        font-style: normal;
+        font-weight: 300;
+        line-height: normal;
+    }
 
 
+    .linguagens{
+        margin-left: 2px
+        position: relative;
+        padding: 2rem;
+        align: right;
+        
+    }
+    .linguagens > h1{
+        position: relative;
+        color: #FFF;
+        font-family: Open Sans;
+        font-size: 32px;
+
+    }
+    .audio-visual{
+        color: #FFF;
+        font-family: Open Sans;
+        font-size: 22px;
+        font-style: normal;
+        font-weight: 300;
+        line-height: normal;
+    }
+    .geral{
+        color: #FFF;
+        font-family: Open Sans;
+        font-size: 22px;
+        font-style: normal;
+        font-weight: 300;
+        line-height: normal;
+    }
+    .microprojeto{
+        color: #FFF;
+        font-family: Open Sans;
+        font-size: 22px;
+        font-style: normal;
+        font-weight: 300;
+        line-height: normal;
+    }
+    .musica{
+        color: #FFF;
+        font-family: Open Sans;
+        font-size: 22px;
+        font-style: normal;
+        font-weight: 300;
+        line-height: normal;
+    }
+
+
+    .suporte{
+        margin-left: 2px
+        position: relative;
+        padding: 2rem;
+        align: right;
+        
+    }
+    .suporte > h1{
+        position: relative;
+        color: #FFF;
+        font-family: Open Sans;
+        font-size: 32px;
+    }
+    .expediente{
+        color: #FFF;
+        font-family: Open Sans;
+        font-size: 22px;
+        font-style: normal;
+        font-weight: 300;
+        line-height: normal;
+    }
+    .ouvidoria{
+        color: #FFF;
+        font-family: Open Sans;
+        font-size: 22px;
+        font-style: normal;
+        font-weight: 300;
+        line-height: normal;
+    }
+    .noticias{
+        color: #FFF;
+        font-family: Open Sans;
+        font-size: 22px;
+        font-style: normal;
+        font-weight: 300;
+        line-height: normal;
+    }
+    .politica-privacidade{
+        color: #FFF;
+        font-family: Open Sans;
+        font-size: 22px;
+        font-style: normal;
+        line-height: normal;
+    }
+    
 `;
