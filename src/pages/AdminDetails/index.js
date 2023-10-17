4b2351422e06c18f0013e169e9411816b0965915
Carlos Lima <144251910+CarlosDev258@users.noreply.github.com>
2023-10-16 21:06:53 -0300

    feat: Start admin profile page

--- src/pages/AdminDetails/index.js
+++ src/pages/AdminDetails/index.js
@@ -1,7 +1,40 @@
-const AdminDetails = () => {
+import Header from "../../components/Header";
+import banner from "../../assets/adminProfile/banner.png";
+import avatar from "../../assets/adminProfile/avatar.jpg";
+import { ProfilePicture, Images, Description } from "./styled";
+import { PencilSquare, PeopleFill } from "react-bootstrap-icons";
+
+function AdminDetails() {
   return (
     <div>
-      <h1>Admin</h1>
+      <Header userName="Roberto Silva" />
+      <img
+        src={banner}
+        alt="Rostos de duas pessoas com máscaras e chapéu coloridos no carnaval"
+      />
+      <Images>
+        <ProfilePicture src={avatar} alt="Foto de perfil do usuário" />
+        <PencilSquare />
+      </Images>
+      <Description>
+        <h1>
+          Roberto Silva
+        </h1>
+        <div>
+        <PeopleFill />
+        <p>
+        MÚSICO PERNAMBUCANO
+        </p>
+        </div>
+        <div>
+        <PencilSquare />
+        <p>
+          Editar
+        </p>
+        </div>
+
+      </Description>
+
     </div>
   );
 }
