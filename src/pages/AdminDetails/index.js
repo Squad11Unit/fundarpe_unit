2ffb5712271d5fb4e1fcc7bdcbeb6b24b417ca89
Artur Ramos <arturramos093@gmail.com>
2023-10-16 23:05:51 -0300

    feat: start second section

--- src/pages/AdminDetails/index.js
+++ src/pages/AdminDetails/index.js
@@ -1,14 +1,26 @@
 import Header from "../../components/Header";
 import banner from "../../assets/adminProfile/banner.png";
 import avatar from "../../assets/adminProfile/avatar.jpg";
-import { ProfilePicture, Images, Description } from "./styled";
+import {
+  Banner,
+  ContainerTable,
+  ProfilePicture,
+  Images,
+  Description,
+  Table,
+  ThOne,
+  ThTwo,
+  ThTree,
+  ThFour,
+  ThFive,
+} from "./styled";
 import { PencilSquare, PeopleFill } from "react-bootstrap-icons";
 
 function AdminDetails() {
   return (
     <div>
       <Header userName="Roberto Silva" />
-      <img
+      <Banner
         src={banner}
         alt="Rostos de duas pessoas com máscaras e chapéu coloridos no carnaval"
       />
@@ -17,24 +29,27 @@ function AdminDetails() {
         <PencilSquare />
       </Images>
       <Description>
-        <h1>
-          Roberto Silva
-        </h1>
+        <h1>Roberto Silva</h1>
         <div>
-        <PeopleFill />
-        <p>
-        MÚSICO PERNAMBUCANO
-        </p>
+          <PeopleFill />
+          <p>MÚSICO PERNAMBUCANO</p>
         </div>
         <div>
-        <PencilSquare />
-        <p>
-          Editar
-        </p>
+          <PencilSquare />
+          <p>Editar</p>
         </div>
-
       </Description>
-
+      <ContainerTable>
+        <Table>
+          <tr>
+            <ThOne>E-mail</ThOne>
+            <ThTwo>Inscrições</ThTwo>
+            <ThTree>Notificações</ThTree>
+            <ThFour>Pareceristas</ThFour>
+            <ThFive>Documentações</ThFive>
+          </tr>
+        </Table>
+      </ContainerTable>
     </div>
   );
 }
--- src/pages/AdminDetails/styled.js
+++ src/pages/AdminDetails/styled.js
@@ -1,5 +1,9 @@
 import styled from "styled-components";
 
+export const Banner = styled.img`
+  width: 100vw;
+`;
+
 export const ProfilePicture = styled.img`
   width: 275px;
   height: 265px;
@@ -21,8 +25,8 @@ export const Images = styled.div`
 export const Description = styled.div`
   display: flex;
   flex-direction: column;
-  margin: -120px 0 0 35%;
-  h1{
+  margin: -120px 0 0 36vw;
+  h1 {
     margin-bottom: 20px;
   }
   div {
@@ -33,10 +37,48 @@ export const Description = styled.div`
       width: 30px;
       height: 30px;
     }
-    p{
-        font-size: 20px;
-        line-height: 20px;
-        margin-top: 0.3rem;
+    p {
+      font-size: 20px;
+      line-height: 20px;
+      margin-top: 0.3rem;
     }
   }
+`;
+
+export const ContainerTable = styled.div`
+  padding: 0 80px;
+`;
+
+export const Table = styled.table`
+  font-size: 20px;
+  width: 100vw;
+  margin-top: 60px;
+  
+`;
+
+export const ThOne = styled.th`
+  border-left: solid 3px red;
+  padding: 0 70px;
+    width: 80%;
+`;
+export const ThTwo = styled.th`
+  border-left: solid 3px yellow;
+  padding: 0 70px;
+    width: 80%;
+`;
+export const ThTree = styled.th`
+  border-left: solid 3px green;
+  padding: 0 70px;
+    width: 80%;
+`;
+export const ThFour = styled.th`
+  border-left: solid 3px #5353ec;
+  padding: 0 70px;
+    width: 80%;
+`;
+export const ThFive = styled.th`
+  border-left: solid 3px blue;
+  padding: 0 70px;
+    width: 80%;
+  border-right: solid 3px orange;
 `;
