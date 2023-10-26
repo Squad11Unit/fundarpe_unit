0392cc34255459aa0c3b0dbf0d5c038ddf6a22ab
ANDERSON FERNANDES <FERNANDES.ANDERSON.DEV@GMAIL.COM>
2023-10-26 14:00:19 -0300

    feat: finaliza formulário de cadastro

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -5,9 +5,12 @@ import LoginHeader from "../../components/LoginHeader";
 import { Acessos } from "../Login/styled";
 import { ContainerRegister } from "./styled";
 import { useNavigate } from "react-router-dom";
+import { cpfMask, validatePassword } from "../../Utils/validations";
 
 const Register = () => {
   const [agente, setAgente] = useState(true);
+  const [form, setForm] = useState([]);
+  const [cpf, setCpf] = useState("");
 
   const navigate = useNavigate();
 
@@ -19,12 +22,26 @@ const Register = () => {
     setAgente(false);
   };
 
-  const handleSubmit = async (event) => {
+  const handleSubmit = (event) => {
     event.preventDefault();
     navigate("/login");
   }
 
- 
+  const handleChange = (event) => {
+    setForm({
+      ...form,
+      [event.target.name]: event.target.value,
+    })
+  }
+
+  const handleCPF = (event) => {
+    setCpf(cpfMask(event.target.value))
+  }
+
+  const InputValidator = () => {
+    return validatePassword(form.password)
+  }
+
   return (
     <div>
       <Header />
@@ -46,61 +63,65 @@ const Register = () => {
             Acesso Administrador
           </h4>
         </Acessos>
-          <Form.Control required size="lg" type="text" placeholder="Nome" />
-          <Form.Control required type="text" size="lg" placeholder="Sobrenome" />
-          <Form.Control required type="text" size="lg" placeholder="País de origem" />
-          <Form.Control required type="text" size="lg" placeholder="CPF" />
-          {!agente && (
-            <Form.Control
-              required
-              type="text"
-              size="lg"
-              placeholder="Matrícula"
-            />
-          )}
-          <Form.Label column lg={20}>
-            Data de nascimento
-          </Form.Label>
+        <Form.Control required size="lg" type="text" placeholder="Nome" />
+        <Form.Control required type="text" size="lg" placeholder="Sobrenome" />
+        <Form.Control required type="text" size="lg" placeholder="País de origem" />
+        <Form.Control required type="text" size="lg" placeholder="CPF" onChange={handleCPF} value={cpf} name={"cpf"} />
+        {!agente && (
           <Form.Control
             required
-            type="date"
+            type="text"
             size="lg"
-            placeholder="Data de nascimento"
+            placeholder="Matrícula"
+            maxLength="11"
           />
-          <Form.Control required type="email" size="lg" placeholder="Email" />
-          <Form.Control
-            required
-            type="password"
-            size="lg"
-            placeholder="Senha"
-          />
-          <span>Mínimo 6 caracteres. Utilize no mínimo uma letra maiúscula, uma minúscula, um caraceter especial e numeros.</span>
-          <Form.Control
-            required
-            aria-required
-            type="password"
-            size="lg"
-            placeholder="Repetir senha"
-          />
-          <Form.Check
-            type="switch"
-            id="costom-switch"
-            label="Autorizo o Funcultura a enviar-me, por qualquer meio, comunicados comerciais e/ou de carácter acadêmico, de forma personalizada e que possam ser do meu interesse, pelo fato de ser usuário da plataforma."
-          />
-          <Form.Check
-            type="switch"
-            id="costom-switch"
-            label="Autorizo o Funcultura a ceder os meus dados a instituições vinculadas a Secretatia de Cultura do Estado de Pernambuco."
-          />
-          <Form.Check
-            required
-           
-            type="switch"
-            id="costom-switch"
-            label="Li e aceito a, Política de privacidade Condições gerais de utilização e Política de Cookies."
-          />
-          <button type="submit">Cadastre-se</button>
-          <p>Já possui uma conta? <a href="/login">Inicie a sessão</a></p>
+        )}
+        <Form.Label column lg={20}>
+          Data de nascimento
+        </Form.Label>
+        <Form.Control
+          required
+          type="date"
+          size="lg"
+          placeholder="Data de nascimento"
+        />
+        <Form.Control required type="email" size="lg" placeholder="Email" />
+        <Form.Control
+          required
+          type="password"
+          name="password"
+          value={form.password}
+          size="lg"
+          placeholder="Senha"
+          onChange={handleChange}
+        />
+        <span>Mínimo 6 caracteres. Utilize no mínimo uma letra maiúscula, uma minúscula, um caraceter especial e numeros.</span>
+        <Form.Control
+          required
+          aria-required
+          type="password"
+          size="lg"
+          placeholder="Repetir senha"
+        />
+        <Form.Check
+          type="switch"
+          id="costom-switch"
+          label="Autorizo o Funcultura a enviar-me, por qualquer meio, comunicados comerciais e/ou de carácter acadêmico, de forma personalizada e que possam ser do meu interesse, pelo fato de ser usuário da plataforma."
+        />
+        <Form.Check
+          type="switch"
+          id="costom-switch"
+          label="Autorizo o Funcultura a ceder os meus dados a instituições vinculadas a Secretatia de Cultura do Estado de Pernambuco."
+        />
+        <Form.Check
+          required
+
+          type="switch"
+          id="costom-switch"
+          label="Li e aceito a, Política de privacidade Condições gerais de utilização e Política de Cookies."
+        />
+        <button type="submit" disabled={!InputValidator()}>Cadastre-se</button>
+        <p>Já possui uma conta? <a href="/login">Inicie a sessão</a></p>
       </ContainerRegister>
     </div>
   );
--- src/pages/Register/styled.js
+++ src/pages/Register/styled.js
@@ -12,15 +12,15 @@ export const ContainerRegister = styled.form`
     font-weight: 700;
    }
    label {
-    margin: -20px 0 -40px;
+    margin: -1.25rem 0 -2.5rem;
    }
    span {
-    margin: -40px 0 -20px;
-    padding-left: 5px;
-    font-size:12px;
+    margin: -2.5rem 0 -1.5rem;
+    padding-left: 0.313rem;
+    font-size: 0.75rem;
    }
    button {
-    width: 292px;
+    width: 18.25rem;
     padding: 0.8rem 1rem;
     margin: 1rem auto 0;
     border: none;
@@ -31,7 +31,20 @@ export const ContainerRegister = styled.form`
     cursor: pointer;
     font-size: 1.5rem;
   }
+  button:disabled {
+    background-color: #d07684;
+    cursor: not-allowed;
+    @media only screen and (max-device-width: 800px) {
+      width: 13.75rem;
+    }
+  }
   p{
     margin: 0 auto;
+    font-weight: 700 ;
+    margin-bottom: 1.25rem;
+  }
+  a{
+    color: black;
+    font-weight: 700 ;
   }
 `;
