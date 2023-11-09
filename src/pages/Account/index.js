29895e58eeae6dcdef1c8316e7cbc4e26ca07691
Beatriz Silva <annbia24@gmail.com>
2023-11-09 14:57:45 -0300

    feat: stepper adjusts

--- src/pages/Account/index.js
+++ src/pages/Account/index.js
@@ -1,21 +1,12 @@
 import Header from "../../components/Header";
 import ProfileHeaders from "../../components/Profiles/header";
 import avatarUser from "../../assets/Profile/avatar-user.jpg";
-import { NoticeSummary, Wrapper, StepperBox, Legend } from "./styled";
+import { NoticeSummary, Wrapper, Legend } from "./styled";
 import cardEdital from "../../assets/Profile/cardEdital.png";
 import cardEdital2 from "../../assets/Profile/pareceristaCard.png";
-import Stepper from "react-stepper-horizontal";
+import { steps, stepsFinished } from "./constantsAccount";
 
 const Account = () => {
-  const steps = [
-    { title: "Inscrição 28/082023 às 15 : 00" },
-    { title: "Submissão" },
-    { title: "Em andamento" },
-    { title: "Habilitado" },
-    { title: "Julgamento/Socialização" },
-    { title: "Entrega documento" },
-  ];
-
   return (
     <Wrapper>
       <Header userName="Ana Silva" />
@@ -38,9 +29,21 @@ const Account = () => {
           <p>Colaborador acompanhe sua inscrição, desejamos-lhe boa sorte!</p>
         </div>
       </NoticeSummary>
-      <StepperBox>
-        <Stepper steps={steps} />
-      </StepperBox>
+      <div className="stepProgressBar st">
+        {steps.map((step) => (
+          <div className="step" key={step.id}>
+            {step.status === "active" ? (
+              <div className="bullet completed" />
+            ) : (
+              <div className="bullet" />
+            )}
+            <div className="stepTitle">{step.title}</div>
+            <div className="stepSubtitle">
+              {step.status === "active" ? step.subTitle : ""}
+            </div>
+          </div>
+        ))}
+      </div>
       <Legend>
         <h3>Legenda</h3>
         <div>
@@ -48,20 +51,36 @@ const Account = () => {
           <h6>Seu processo foi aprovado nessa etapa</h6>
         </div>
         <div>
-          <p />
+          <p className="wait" />
           <h6>Seu processo ainda não entrou nessa etapa</h6>
         </div>
       </Legend>
-      <h2>
-        Histórico
-      </h2>
+      <h2>Histórico</h2>
       <NoticeSummary>
         <img src={cardEdital2} alt="Card do edital" />
         <div>
           <h3>EDITAL DE CREDENCIAMENTO DE PARECERISTA</h3>
-          <p>Colaborador sua inscrição foi finalizada, desejamos-lhe boa sorte!</p>
+          <p>
+            Colaborador sua inscrição foi finalizada, desejamos-lhe boa sorte!
+          </p>
         </div>
       </NoticeSummary>
+      <div className="stepProgressBar st">
+        {stepsFinished.map((step) => (
+          <div className="step" key={step.title}>
+            {step.status === "active" ? (
+              <div className="bullet completed" />
+            ) : (
+              <div className="bullet" />
+            )}
+            <div className="stepTitle">{step.title}</div>
+            <div className="stepSubtitle">
+              {step.status === "active" ? step.subtitle : ""}
+            </div>
+          </div>
+        ))}
+      </div>
+
     </Wrapper>
   );
 };
--- src/pages/Account/styled.js
+++ src/pages/Account/styled.js
@@ -1,9 +1,63 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.div`
+  padding-bottom: 5rem;
   h2 {
     margin: 6rem 0 0 6rem;
     font-size: 2.25rem;
+    clear: both;
+  }
+  .stepProgressBar {
+    display: flex;
+    justify-content: space-between;
+    align-item: flex-start;
+    width: 90%;
+    margin: 4rem auto 0;
+    .step {
+      text-align: center;
+      width: 20%;
+      position: relative;
+    }
+    .stepTitle {
+      margin-bottom: 0.625rem;
+      font-weight: 600;
+    }
+    .stepSubtitle {
+      font-size: 0.8rem;
+    }
+    .bullet {
+      background: #a0a0a0;
+      border-radius: 50%;
+      width: 2rem;
+      height: 2rem;
+      display: inline-block;
+      transition: background-color 500ms;
+      line-height: 1.25rem;
+    }
+    .bullet.completed {
+      background: #30ae61;
+    }
+    .bullet.completed::after {
+      content: "_______________";
+      color: #30ae61;
+      font-size: 1.5rem;
+      position: absolute;
+      top: 0.5rem;
+      left: 100%;
+      transform: translate(-50%, -50%);
+    }
+    .bullet::after {
+      content: "_______________";
+      color: #a0a0a0;
+      font-size: 1.5rem;
+      position: absolute;
+      top: 0.5rem;
+      left: 100%;
+      transform: translate(-50%, -50%);
+    }
+    .step:last-child .bullet::after {
+      content: "";
+    }
   }
 `;
 
@@ -18,24 +72,31 @@ export const NoticeSummary = styled.div`
   }
 `;
 
-export const StepperBox = styled.div`
-margin-top: 5rem;
-
-`;
-
 export const Legend = styled.div`
-margin-top: 6rem;
-display: flex;
-flex-direction: column;
-div{
-display:flex;
-gap: 0.7rem;
-
-p{
-   background: #30ae61;
-   border-radius: 50%;
-   width: 30px;
-   height: 30px;
+  margin: 6rem 5rem 6rem 0;
+  display: inline-flex;
+  flex-direction: column;
+  float: right;
+  h3 {
+    font-size: 1.5rem;
+    font-weight: 700;
+    margin-bottom: 1rem;
   }
-}
-`
+  h6 {
+    margin-top: 0.3rem;
+  }
+  div {
+    display: flex;
+    gap: 0.7rem;
+
+    p {
+      background: #30ae61;
+      border-radius: 50%;
+      width: 30px;
+      height: 30px;
+    }
+    .wait{
+      background: #A0A0A0;
+    }
+  }
+`;
